37faa78ed66449567a49b002230db80508745424
zaingomber <116724134+zaingomber@users.noreply.github.com>
2026-05-11 12:15:37 +0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'ok-bos-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch dari Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Gunakan cache jika ada (mempercepat loading)
+        }
+        return fetch(event.request);
+      })
+  );
+});
